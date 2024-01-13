308791f2b9e243010b97f09fdabdda7a6525d09d
yaikohi <erik.beem@hotmail.com>
2024-01-13 19:49:03 +0100

    good enough

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,79 @@
-import { Elysia } from "elysia";
+import { Elysia, t } from "elysia";
+import { PostDBType, PostType } from "./types";
+import cors from "@elysiajs/cors";
+// --- POST
+const PORT = 4000;
+// --- CONSTS
+const DB: PostDBType = {};
+const posts = (): PostType[] => {
+  return Object.entries(DB).map(([k, v]) => ({
+    id: k,
+    title: v.title,
+    content: v.content,
+    comments: v.comments,
+  }));
+};
 
-const app = new Elysia().get("/", () => "Hello Elysia").listen(3000);
+// -- APP
+const app = new Elysia();
+// --- MIDDLEWARE
+app
+  .use(cors());
+// --- ROUTES
+app
+  .get("/", () => "Hello Elysia")
+  .group("/events", (app) =>
+    app
+      .post("/", ({ body, set }) => {
+        set.status = "OK";
+        console.log(body);
+      }))
+  .group("/posts", (app) =>
+    app
+      .get("/", () => posts)
+      .post("/", async ({ body, set }) => {
+        const postId = crypto.randomUUID();
+        const newPost: PostType = {
+          id: postId,
+          title: body.title,
+          content: body.content,
+          comments: [],
+        };
 
-console.log(
-  `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`
-);
+        DB[postId] = newPost;
+
+        await sendPostCreatedEvent({ post: newPost });
+
+        set.status = "OK";
+
+        return { success: true, message: "Post was created." };
+      }, {
+        body: t.Object({
+          title: t.String(),
+          content: t.String(),
+        }),
+      }));
+
+// --- START APP
+app
+  .listen(PORT, () =>
+    console.log(
+      `🦊 Elysia is running the 'posts' service at ${app.server?.hostname}:${app.server?.port}`,
+    ));
+
+export async function sendPostCreatedEvent({ post }: { post: PostType }) {
+  const url = `http://localhost:4005/events`;
+  const event = { type: "post.created", data: { post } };
+
+  try {
+    await fetch(url, {
+      method: "POST",
+      body: JSON.stringify(event),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+  } catch (err) {
+    console.error(err);
+  }
+}
---
+++ src/types.ts
@@ -0,0 +1,31 @@
+// --- TYPES
+export type PostDBType = {
+  [key: string]: PostType;
+};
+export type PostType = {
+  id: string;
+  title: string;
+  content: string;
+  comments: CommentType[] | [];
+};
+export type CommentType = {
+  id: string;
+  content: string;
+  status: "PENDING" | "REJECTED" | "APPROVED";
+};
+export type PostEventType = {
+  type:
+  | "post.created"
+  | "post.updated"
+  | "post.deleted";
+  data: PostType;
+};
+export type CommentEventType = {
+  type:
+  | "comment.created"
+  | "comment.updated"
+  | "comment.moderated"
+  | "comment.deleted";
+  data: { comment: CommentType; postId: string };
+};
+export type EventType = PostEventType | CommentEventType;
