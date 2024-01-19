7c9f5a06fb1fdfde41744a4bfd1df2c89410e018
yaikohi <erik.beem@hotmail.com>
2024-01-19 20:22:09 +0100

    ??

--- src/utils.ts
+++ src/utils.ts
@@ -5,7 +5,7 @@ export const getURL = (): string => {
   const HOST_EVENTBUS = `event-bus-srv`;
   const URL_EVENTBUS = `http://${HOST_EVENTBUS}:${PORT_EVENTBUS}/events`;
 
-  const localURL = `http://localhost:4005/events`;
+  const localURL = `http://localhost:${PORT_EVENTBUS}/events`;
 
   if (env.NODE_ENV === "development") {
     return localURL;
